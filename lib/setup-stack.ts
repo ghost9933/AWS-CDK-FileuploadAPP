9f81c378a403563394494a73a5ff5280595d24a9
ghost9933 <mhatrenikhil36@gmail.com>
2024-04-06 18:40:38 -0500

    Add index.html in S3

--- lib/setup-stack.ts
+++ lib/setup-stack.ts
@@ -1,12 +1,59 @@
 import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import { Construct } from '@aws-cdk/core';
 
-export class MyCdkProjectStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3'; // Update import statement
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'; // Update import statement
+import { AllowedMethods, Distribution, OriginAccessIdentity, ViewerProtocolPolicy } from '@aws-cdk/aws-cloudfront'; // Update import statement
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins"; // Update import statement
 
-        new s3.Bucket(this, 'MyBucket', {
-            versioned: true,
-        });
-    }
+export class FileUploadStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.createFrontendResources();
+  }
+
+  private createFrontendResources() {
+    const frontendBucket = this.createS3Bucket();
+    this.deployS3Bucket(frontendBucket);
+    const s3Origin = this.createOriginAccess(frontendBucket);
+    this.createCloudFrontDistribution(s3Origin);
+  }
+
+  private createS3Bucket(): Bucket { // Update return type to Bucket
+    return new Bucket(this, 'FrontendBucket', {
+      bucketName: 'frontend',
+      encryption: BucketEncryption.S3_MANAGED,
+      publicReadAccess: false
+    });
+  }
+
+  private deployS3Bucket(frontendBucket: Bucket) { // Update parameter type to Bucket
+    new BucketDeployment(this, 'DeployFrontend', {
+      sources: [Source.asset('./app')], // Adjust the path if needed
+      destinationBucket: frontendBucket
+    });
+  }
+
+  private createOriginAccess(frontendBucket: Bucket): S3Origin { // Update parameter type to Bucket
+    const originAccess = new OriginAccessIdentity(this, 'OriginAccessControl', { comment: 'AWS To-do App OAI' });
+    return new S3Origin(frontendBucket, { originAccessIdentity: originAccess });
+  }
+
+  private createCloudFrontDistribution(s3Origin: S3Origin): Distribution {
+    return new Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: s3Origin,
+        viewerProtocolPolicy: ViewerProtocolPolicy.ALLOW_ALL,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responsePagePath: '/index.html',
+          responseHttpStatus: 200
+        }
+      ],
+    });
+  }
 }
