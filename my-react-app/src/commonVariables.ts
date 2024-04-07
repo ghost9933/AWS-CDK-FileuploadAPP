3e355296ad760144b9c5e3c6504e60b61e0e7ebe
ghost9933 <mhatrenikhil36@gmail.com>
2024-04-07 12:36:42 -0500

    fxed dependencie error

--- my-react-app/src/commonVariables.ts
+++ my-react-app/src/commonVariables.ts
@@ -1,5 +1,5 @@
 // commonVariables.ts
 
 export const S3StorageBucketName = 'storagebucket-fovus';
-export const S3WebBucketName = 'webbucket';
+export const S3WebBucketName = 'webbuckethost';
 export const FileTableName = 'MyFileTable';
