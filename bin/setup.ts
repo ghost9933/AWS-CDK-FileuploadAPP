550cff96793051247922273a04b91f469d85e52b
ghost9933 <mhatrenikhil36@gmail.com>
2024-04-06 21:16:33 -0500

    deployin test app to s3

--- bin/setup.ts
+++ bin/setup.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
+
 import { MyCdkProjectStack } from '../lib/setup-stack';
 
-const app = new cdk.App();
-new MyCdkProjectStack(app, 'MyCdkProjectStack');
+const app = new App();  
+
+new MyCdkProjectStack(app,'MyCdkProjectStack');
--- lib/setup-stack.ts
+++ lib/setup-stack.ts
@@ -1,59 +1,69 @@
-import * as cdk from '@aws-cdk/core';
-import { Construct } from '@aws-cdk/core';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { CloudFrontWebDistribution } from 'aws-cdk-lib/aws-cloudfront';
 
-import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3'; // Update import statement
-import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'; // Update import statement
-import { AllowedMethods, Distribution, OriginAccessIdentity, ViewerProtocolPolicy } from '@aws-cdk/aws-cloudfront'; // Update import statement
-import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins"; // Update import statement
+export class MyCdkProjectStack extends Stack {
 
-export class MyCdkProjectStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    public readonly bucket: Bucket; 
+    public readonly distribution: CloudFrontWebDistribution;
+    public readonly cloudfrontDomainName: CfnOutput;
+    public readonly s3BucketUrl: CfnOutput;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    this.createFrontendResources();
+    this.bucket = this._createS3Bucket();
+    this.distribution = this._createCloudFrontDistribution(this.bucket);
+    this.cloudfrontDomainName = this._outputDomainName(this.distribution);
+    this.s3BucketUrl = this._outputS3Url(this.bucket);
   }
 
-  private createFrontendResources() {
-    const frontendBucket = this.createS3Bucket();
-    this.deployS3Bucket(frontendBucket);
-    const s3Origin = this.createOriginAccess(frontendBucket);
-    this.createCloudFrontDistribution(s3Origin);
-  }
-
-  private createS3Bucket(): Bucket { // Update return type to Bucket
-    return new Bucket(this, 'FrontendBucket', {
-      bucketName: 'frontend',
-      encryption: BucketEncryption.S3_MANAGED,
-      publicReadAccess: false
+  _createS3Bucket() {
+    const reactDeploymentBucket = new Bucket(this, 'ReactDeploymentBucket', {
+      bucketName: 'react-deployment-bucket', //should be unique
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: true,
     });
+
+    return reactDeploymentBucket;
   }
 
-  private deployS3Bucket(frontendBucket: Bucket) { // Update parameter type to Bucket
-    new BucketDeployment(this, 'DeployFrontend', {
-      sources: [Source.asset('./app')], // Adjust the path if needed
-      destinationBucket: frontendBucket
+  _createCloudFrontDistribution(bucket: Bucket) {
+    const distribution = new CloudFrontWebDistribution(
+      this,
+      'ReactDeploymentDistribution',
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    return distribution;
+  }
+
+  _outputS3Url(reactDeploymentBucket: Bucket) {
+    const s3Url = new CfnOutput(this, 'WebsiteURL', {
+      value: reactDeploymentBucket.bucketWebsiteUrl,
+      description: 'URL of the website',
     });
+
+    return s3Url;
   }
 
-  private createOriginAccess(frontendBucket: Bucket): S3Origin { // Update parameter type to Bucket
-    const originAccess = new OriginAccessIdentity(this, 'OriginAccessControl', { comment: 'AWS To-do App OAI' });
-    return new S3Origin(frontendBucket, { originAccessIdentity: originAccess });
-  }
-
-  private createCloudFrontDistribution(s3Origin: S3Origin): Distribution {
-    return new Distribution(this, 'Distribution', {
-      defaultBehavior: {
-        origin: s3Origin,
-        viewerProtocolPolicy: ViewerProtocolPolicy.ALLOW_ALL,
-        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS
-      },
-      errorResponses: [
-        {
-          httpStatus: 403,
-          responsePagePath: '/index.html',
-          responseHttpStatus: 200
-        }
-      ],
+  _outputDomainName(distribution: CloudFrontWebDistribution) {
+    const domainName = new CfnOutput(this, 'CloudFrontDistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'CloudFront Distribution Domain Name',
     });
+
+    return domainName;
   }
 }
